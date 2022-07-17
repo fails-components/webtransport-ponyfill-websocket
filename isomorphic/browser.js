48201cea609ca4e97e5791c8f96056de8ebb1422
Marten Richter <marten.richter@tu-berlin.de>
2022-07-17 14:33:33 +0200

    Fix stream polyfill for webworker

---
+++ isomorphic/browser.js
@@ -0,0 +1,5 @@
+// Copyright (c) 2022 Marten Richter or other contributers (see commit). All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+// eslint-disable-next-line no-undef
+export const { ReadableStream, WritableStream } = self
